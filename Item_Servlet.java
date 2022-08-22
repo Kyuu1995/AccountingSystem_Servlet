2fe5cb6002ecc353b84e3f02f4e97eab38a1ed66
Kyuu1995 <111324748+Kyuu1995@users.noreply.github.com>
2022-08-22 15:47:16 +0800

    開始撰寫Servlet

---
+++ Item_Servlet.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/doItemServlet")
+public class Item_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String date = request.getParameter("date");
+		String item = request.getParameter("item");
+		int amount = Integer.parseInt(request.getParameter("amount"));
+		String notes = request.getParameter("notes");
+		System.out.print(date + item + amount + notes);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
